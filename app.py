6ce52c8602dd9f9b4f7ecfdc34c55dfe71497d71
oriexsol <62597130+oriexsol@users.noreply.github.com>
2022-04-30 22:02:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-from flask import Flask
-from datetime import datetime
-import socket
 from requests import get
 import platform
 import os
@@ -19,31 +16,34 @@ def welcome():
 
 if __name__ == '__main__':
     Tm = str((datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
-    Tm = "The time is: " + Tm
+    Tm = "Server time is: " + Tm
 
     InternalIP = str(socket.gethostbyname(socket.gethostname()))
-    InternalIP = "The InternalIP is: " + InternalIP
+    InternalIP = "Server InternalIP is: " + InternalIP
 
 
-    
+
     ip = get('https://api.ipify.org').content.decode('utf8')
-    ExternalIP = ('My public IP address is: {}'.format(ip))
+    ExternalIP = ('Server public IP address is: {}'.format(ip))
 
-    CPU_Model = "My CPU Model is: " + platform.machine()
+    CPU_Model = "Server CPU Model is: " + platform.machine()
     print(CPU_Model)
 
-    CPU_Cores = "The number of CPU Cores is: " + str(os.cpu_count())
+    CPU_Cores = "Server number of CPU Cores is: " + str(os.cpu_count())
 
-#    gpus = GPUtil.getGPUs()
-#    if gpus:
-#        for gpu in gpus:
-#	    gpuname = gpu.name
-#        GPU_Type = "My GPU Type is: " + str(gpuname)
+    gpus = GPUtil.getGPUs()
+    GpuA=""
+    if gpus:
+        for gpu in gpus:
+            GpuA=str(gpu.name)
+    else:
+        GpuA="No GPUs"
+    GPU_Type = "Server GPU Type is: " + GpuA
 
-    Memory = "The size of the Memory is: " + str((psutil.virtual_memory()[0])/1024/1024/1024)[0:4] + "Gb"
+    Memory = "Server size of the Memory is: " + str((psutil.virtual_memory()[0])/1024/1024/1024)[0:4] + "Gb"
 
-    data = Tm + "<br/>" + InternalIP + "<br/>" + ExternalIP + "<br/>" + CPU_Model + "<br/>" + CPU_Cores  + "<br/>" + Memory
-
+    data = Tm + "<br/>" + InternalIP + "<br/>" + ExternalIP + "<br/>" + CPU_Model + "<br/>" + CPU_Cores + "<br/>" + GPU_Type + "<br/>" + Memory
+    
     # data = Tm + "\n" + InternalIP + "\n" + ExternalIP + "\n" + CPU_Model + "\n" + CPU_Cores + "\n" + GPU_Type + "\n" + Memory
 
     app.run(host='0.0.0.0', port=80)  
