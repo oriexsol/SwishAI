5002f73d1e37b41dc1ffdb403f7f430c3f40f817
oriexsol <62597130+oriexsol@users.noreply.github.com>
2022-05-01 10:53:55 +0300

    Create kube-master-start.sh

---
+++ kube-master-start.sh
@@ -0,0 +1,7 @@
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl apply -f https://github.com/coreos/flannel/raw/master/Documentation/kube-flannel.yml
